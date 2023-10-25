f72c39595883625336213ec29473d549d84638fc
Abhishek gupta <abhishekgupta8909@gmail.com>
2023-10-25 17:44:50 +0530

    db conn updated

--- src/api/config/config.js
+++ src/api/config/config.js
@@ -1,8 +1,5 @@
 
 module.exports =  {
-    "username": process.env.MYSQL_USERNAME,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DATABASE,
-    "host": process.env.MYSQL_HOST,
+    "databaseUrl": process.env.MYSQL_DATABASE_URL,
     "dialect": process.env.MYSQL_DIALECT
 }
--- src/api/db.js
+++ src/api/db.js
@@ -2,9 +2,22 @@
 
 
 const { Sequelize } = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config');
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(config.databaseUrl, {
+  dialect: config.dialect,
+  logging: false
+});
+
+async function testDatabaseConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testDatabaseConnection();
 
 module.exports = sequelize;
