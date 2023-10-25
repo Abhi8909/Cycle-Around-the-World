ed19658065a95c72fa501bfb9fe014f6e16c8d6d
Abhishek gupta <abhishekgupta8909@gmail.com>
2023-10-25 17:19:50 +0530

    env updated

---
+++ src/api/config/config.js
@@ -0,0 +1,8 @@
+
+export default config = {
+    "username": process.env.MYSQL_USERNAME,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "dialect": process.env.MYSQL_DIALECT
+}
--- src/api/db.js
+++ src/api/db.js
@@ -3,7 +3,7 @@
 
 const { Sequelize } = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require('./config/config.json')[env];
+const config = require('./config/config');
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
